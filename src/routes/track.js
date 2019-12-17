0fc25de115649f5db4ab31a3dc7a38adac273236
Serere Jegede <sererejegede@gmail.com>
2019-12-17 16:17:59 +0100

    Added update and delete

--- src/routes/track.js
+++ src/routes/track.js
@@ -7,11 +7,13 @@ const router = express.Router();
 
 router.use(authGuard);
 
+// Get user tracks
 router.get('/track', async (req, res) => {
   const tracks = await Track.find({user_id: req.user._id});
   return res.json({data: tracks}).status(200)
 });
 
+// Create new track
 router.post('/track', async (req, res) => {
   const { name, locations } = req.body;
   if (!name || !locations) {
@@ -29,5 +31,32 @@ router.post('/track', async (req, res) => {
   })
 
 });
+
+// Update track
+// TODO add location update functionality
+router.put('/track/:_id', async (req, res) => {
+  const track = await Track.findById(req.params._id)
+  if (!track) {
+    return res.status(422).json({
+      error: 'Could not find track'
+    })
+  }
+  track.name = req.body.name
+  await track.save()
+
+  res.status(200).json({
+    message: 'Saved successfully',
+    data: track
+  })
+})
+
+router.delete('/track/:_id', async (req, res) => {
+  const status = await Track.findByIdAndDelete(req.params._id)
+  if (status) {
+    res.status(204).json({
+      message: 'Deleted Successfully'
+    })
+  }
+})
 
 module.exports = router;
