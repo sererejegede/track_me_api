5ac67e2e8842c0253ec6bcd60755ca2f34e94389
sererejegede <sererejegede@gmail.com>
2020-01-16 17:46:11 +0100

    feat: Added mock for Material

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('./models/User');
 require('./models/Track');
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authGuard = require('./middlewares/authGuard');
@@ -8,25 +9,28 @@ const authGuard = require('./middlewares/authGuard');
 const authRoutes = require('./routes/auth');
 const trackRoutes = require('./routes/track');
 
+const materialRoutes = require('./routes/material');
+
 // Mongo Setup
-const mongoURI =
-  'mongodb+srv://user_first:bP2Fwt6VjD2Zg6z@cluster0-34vnh.mongodb.net/test?retryWrites=true&w=majority';
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
-mongoose.connection.on('connected', () =>
-  console.log('Conected to Mongodb...')
-);
-mongoose.connection.on('error', err =>
-  console.error('Error connecting to Mongodb', err)
-);
+// const mongoURI =
+//   'mongodb+srv://user_first:bP2Fwt6VjD2Zg6z@cluster0-34vnh.mongodb.net/test?retryWrites=true&w=majority';
+// mongoose.connect(mongoURI, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true
+// });
+// mongoose.connection.on('connected', () =>
+//   console.log('Conected to Mongodb...')
+// );
+// mongoose.connection.on('error', err =>
+//   console.error('Error connecting to Mongodb', err)
+// );
 
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(cors());
 // Define routes here
+app.use(materialRoutes);
 app.use(authRoutes);
 app.use(trackRoutes);
 
---
+++ src/routes/material.js
@@ -0,0 +1,192 @@
+const express = require('express');
+
+const router = express.Router();
+const Status = {
+  PUBLISHED: 'PUBLISHED',
+  DRAFTED: 'DRAFTED'
+}
+const materials = [
+  {
+    key: 1,
+    productNumber: 'EKI 1300 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 2,
+    productNumber: 'Eki 108 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 3,
+    productNumber: 'EKI 5500 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 4,
+    productNumber: 'EKI 128 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.DRAFTED
+  },
+  {
+    key: 5,
+    productNumber: 'EKI 129 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 6,
+    productNumber: 'EKI 121 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5, S6, S7',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 7,
+    productNumber: 'EKI 250 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S3, S4, S5, S6',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 8,
+    productNumber: 'EKI 251 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 9,
+    productNumber: 'EKI 252 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 10,
+    productNumber: 'EKI 255 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 11,
+    productNumber: 'Eki 107 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5, S6',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 12,
+    productNumber: 'Eki 108 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 13,
+    productNumber: 'EKI 5500 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 14,
+    productNumber: 'EKI 128 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.DRAFTED
+  },
+  {
+    key: 15,
+    productNumber: 'EKI 129 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 16,
+    productNumber: 'EKI 121 PU',
+    category: 'Polyetherschuim',
+    shapes: 'S1, S2, S3, S4, S5, S6, S7',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 17,
+    productNumber: 'EKI 250 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S3, S4, S5, S6',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 18,
+    productNumber: 'EKI 251 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 19,
+    productNumber: 'EKI 252 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.PUBLISHED
+  },
+  {
+    key: 20,
+    productNumber: 'EKI 255 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.DRAFTED
+  },
+  {
+    key: 21,
+    productNumber: 'EKI 255 SBR',
+    category: 'SBR Rubber',
+    shapes: 'S1, S2, S3, S4, S5',
+    status: Status.DRAFTED
+  }
+];
+
+const filterData = (searchTerm, data) => {
+  return data.filter(
+    datum =>
+      datum.category
+        .toLowerCase()
+        .indexOf(searchTerm.toLowerCase()) !== -1 ||
+      datum.productNumber
+        .toLowerCase()
+        .indexOf(searchTerm.toLowerCase()) !== -1 ||
+      datum.status.toLowerCase().indexOf(searchTerm.toLowerCase()) !==
+        -1
+  )
+}
+
+router.get('/material', async (req, res) => {
+  let data = materials
+  let pageCount = Math.ceil(data.length / 10);
+  let page = parseInt(req.query.page);
+  if (!page) { page = 1;}
+  if (page > pageCount) {
+    page = pageCount
+  }
+  if (req.query.searchTerm) {
+    data = filterData(req.query.searchTerm, materials)
+    pageCount = Math.ceil(data.length / 10);
+  }
+  return res.json({
+    data: data.slice(page * 10 - 10, page * 10),
+    pageCount,
+    page,
+    total: data.length
+  })
+});
+
+module.exports = router;
